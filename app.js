87879910b2ac9c5c9b4686fad9d2de04f6faa198
Edgar <edgarvazevedo@gmail.com>
2021-11-29 20:20:52 +0100

    first commit

--- app.js
+++ app.js
@@ -6,6 +6,22 @@ const PORT = 4000;
 const allPokemon = require("./data");
 
 const app = express();
+app.use(express.json());
+
+app.get("/pokemon", (req, res) => {
+  return res.status(200).json(allPokemon);
+});
+
+app.get("/pokemon", (req, res) => {
+  const foundPokemon = allPokemon.find((poke) => {
+    return String(poke.id) === req.params.id;
+  });
+
+  if (foundPokemon) {
+    return res.status(200).json(foundPokemon);
+  }
+  return res.status(404).json({ msg: "Pokemon não encontrado" });
+});
 
 // -- Define your route listeners here! --
 
